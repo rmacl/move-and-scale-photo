76bffee86a11067e38e202ee5dddffb377aabb60
tlrmacl <catasy@naver.com>
2019-09-08 23:08:55 -0700

    delete not used import line of code

--- src/screen/MoveScale.tsx
+++ src/screen/MoveScale.tsx
@@ -1,15 +1,16 @@
 
-import React , { useState, useEffect  } from 'react';
-import { StyleSheet, View, TouchableOpacity, Image, Dimensions  } from 'react-native';
+import React  from 'react';
+import { StyleSheet, View, Image, Dimensions  } from 'react-native';
 import MoveAndScale from '../component/moveAndScale';
 
 
 export default function MoveScale(props){
+    console.log('d');
     let imgLocation =  props.navigation.state.params.imageUrl;   
         return (
          <View style ={styles.container} >
-         <MoveAndScale screenWidth={Dimensions.get('window').width}
-                       screenHeight={Dimensions.get('window').height}
+         <MoveAndScale screenWidth={400}
+                       screenHeight={400}
                    >
              <Image style={{width:300, height: 300}}
                    source={{uri:imgLocation}}/>
